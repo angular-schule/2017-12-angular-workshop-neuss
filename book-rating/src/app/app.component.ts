27e0c643acb712dc0ba8b75f2524c503a396fab6
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-12-12 13:48:42 +0100

    changing a bit the start html

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'br';
+  title = 'Book Rating';
+
+  constructor() {
+    console.log('Hallo');
+  }
+
 }
