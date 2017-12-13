9ce801dddfcf36e5a5a2afa15bdbb1becb653f4f
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-12-13 21:05:23 +0100

    e2e test:  Audatex website should display the important news

--- book-rating/e2e/app.e2e-spec.ts
+++ book-rating/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('book-rating App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to br!');
+    expect(page.getParagraphText()).toEqual('Test Book Rating');
   });
 });
