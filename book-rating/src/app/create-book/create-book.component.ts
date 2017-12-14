7b1cfb027c2e3f27e6b15aa8a471b473bca82293
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-12-14 12:37:51 +0100

    reseting validation

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -1,5 +1,6 @@
+import { FormGroup } from '@angular/forms';
 import { Book } from './../shared/book';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'br-create-book',
@@ -10,11 +11,15 @@ export class CreateBookComponent {
 
   book = Book.empty();
 
+  @ViewChild('bookForm')
+  bookForm: FormGroup;
+
   @Output()
   bookCreated = new EventEmitter<Book>();
 
   add() {
     this.bookCreated.emit(this.book);
     this.book = Book.empty();
+    this.bookForm.reset(this.book);
   }
 }
